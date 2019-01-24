19b504374fce7df2f9d2579e7d56713f669db6f2
John Lundgren <john.paul.lundgren@gmail.com>
2019-01-24 16:51:23 +0100

    Module 3 finished.

---
+++ WiredBrainCoffee/Pages/Item.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WiredBrainCoffee.Models;
+using WiredBrainCoffee.Services;
+
+namespace WiredBrainCoffee.Pages
+{
+    public class ItemModel : PageModel
+    {
+        public MenuItem Item { get; private set; }
+
+        public void OnGet(int id)
+        {
+            var service = new MenuService();
+            Item = service.GetMenuItems().FirstOrDefault(i => i.Id == id);
+        }
+    }
+}
